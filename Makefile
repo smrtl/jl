be143720b591b0a67ea9623a7ecf47094c1cad06
Samuel Suter <samuel.suter@nagra.com>
2024-09-26 18:41:56 +0200

    initial implementation

---
+++ Makefile
@@ -0,0 +1,104 @@
+# py project metadata
+PYPROJECT_NAME?=$(shell awk -F\" '/^name *=/ { print $$2; exit; }' pyproject.toml)
+PYPROJECT_VERSION?=$(shell awk -F\" '/^version *=/ { print $$2; exit; }' pyproject.toml)
+
+# poetry (in-project virtual env name is `.venv`)
+POETRY?=$(shell command -v poetry 2> /dev/null)
+POETRY_RUN?=$(POETRY) run
+POETRY_LOCK?=poetry.lock
+POETRY_VENV?=.venv
+POETRY_VENV_STAMP?=$(POETRY_VENV)/.install.stamp
+POETRY_SETUP_COMMAND?=install
+ifdef CI
+POETRY_OUTPUT_ARGS?=--no-ansi
+endif
+
+# paths
+SRC_DIR?=$(PYPROJECT_NAME)
+TESTS_DIR?=tests
+
+.PHONY: help
+help: ## This help message
+	@grep -E '^[a-zA-Z0-9/_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sed 's/.*Makefile[^:]*://g' | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+$(POETRY_LOCK):
+$(POETRY_VENV_STAMP): pyproject.toml poetry.toml $(POETRY_LOCK)
+	@if [ -z $(POETRY) ]; then echo Poetry could not be found. See https://python-poetry.org/docs/; exit 2; fi
+	@$(POETRY) $(POETRY_SETUP_COMMAND) $(POETRY_OUTPUT_ARGS) && \
+		touch $(POETRY_VENV_STAMP)
+
+.PHONY: venv/create
+venv/create: $(POETRY_VENV_STAMP) ## setup the python virtual environment
+
+.PHONY: venv/lock
+venv/lock: ## udpates the poetry lock file
+	@$(POETRY) lock
+
+.PHONY: venv/update
+venv/update: venv/create ## udpates the python dependencies
+	@$(POETRY) update
+
+.PHONY: venv/add
+venv/add: venv/create ## add a python dependency (must specify the DEP var)
+	@$(POETRY) add $(DEP)
+
+.PHONY: venv/add/dev
+venv/add/dev: venv/create ## add a python dev dependency (must specify the DEP var)
+	@$(POETRY) add -G dev $(DEP)
+
+.PHONY: venv/outdated
+venv/outdated: venv/create ## list outdated dependencies
+	@$(POETRY) show -oT
+
+.PHONY: venv/clean
+venv/clean: ## clean the python virtual environment
+	@rm -rf $(POETRY_VENV)
+
+.PHONY: cache/clean
+cache/clean: ## clean local caches (mypy, pytest, etc.)
+	@rm -rf .api-doc .mypy_cache .pytest_cache .ruff_cache && \
+		find . -type d -name __pycache__ -print0 | xargs -0 rm -rf
+
+.PHONY: clean
+clean: venv/clean doc/venv/clean cache/clean ## clean the dev environment (venv & caches)
+
+.PHONY: lint/style
+lint/style: venv/create ## run formatting linters
+	@$(POETRY_RUN) black --check $(SRC_DIR) $(TESTS_DIR) && \
+		$(POETRY_RUN) ruff check $(SRC_DIR) $(TESTS_DIR)
+
+.PHONY: lint/type
+lint/type: venv/create ## run the static type checking
+	@$(POETRY_RUN) mypy --strict $(SRC_DIR) $(TESTS_DIR)
+
+.PHONY: lint
+lint: lint/style lint/type ## run linting
+
+.PHONY: lint/fix
+lint/fix: venv/create ## run linting and fix issues
+	@$(POETRY_RUN) black $(SRC_DIR) $(TESTS_DIR) && \
+		$(POETRY_RUN) ruff check --fix --extend-select I001 $(SRC_DIR) $(TESTS_DIR)
+
+.PHONY: test
+test: ONLY?=*
+test: venv/create ## run unit tests
+	@$(POETRY_RUN) pytest -q $(TESTS_DIR)/test_*$(ONLY)*.py
+
+.PHONY: test/verbose
+test/verbose: ONLY?=*
+test/verbose: ## run unit tests with more verbosity
+	@$(POETRY_RUN) pytest -s $(TESTS_DIR)/test_*$(ONLY)*.py
+
+.PHONY: test/watch
+test/watch: ONLY?=*
+test/watch: ## run unit tests on every code change
+	@$(POETRY_RUN) ptw --clear --now --patterns '$(SRC_DIR)/*.py,$(SRC_DIR)/**/*.py,$(TESTS_DIR)/*.py' . \
+		$(TESTS_DIR)/test_*$(ONLY)*.py --ignore=$(SRC_DIR) -q
+
+.PHONY: build
+build: venv/create ## build the project (wheel & source archive)
+	@$(POETRY) build
+
+.PHONY: version
+version: ## output the current version of the project
+	@echo $(PYPROJECT_VERSION)
---
+++ jl/cli.py
@@ -0,0 +1,119 @@
+import json
+import logging
+from typing import IO, Any
+
+import click
+from dateutil.parser import ParserError, parse
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("jl")
+
+LOG_LEVELS = {
+    "TRACE": ("TRACE", "cyan"),
+    "DEBUG": ("DEBUG", "blue"),
+    "INFO": ("INFO", "green"),
+    "WARN": ("WARN", "yellow"),
+    "WARNING": ("WARN", "yellow"),
+    "ERROR": ("ERROR", "red"),
+    "CRITICAL": ("CRIT", "red"),
+    "FATAL": ("FATAL", "red"),
+}
+
+
+def parse_json(line: str) -> tuple[str | None, dict[str, Any] | None]:
+    json_start = line.find("{")
+    try:
+        if json_start > -1:
+            data = json.loads(line[json_start:])
+            return line[0:json_start], data
+    except json.JSONDecodeError as e:
+        logger.debug("%s", e)
+    return None, None
+
+
+def get_value(data: dict[str, Any], keys: tuple[str, ...]) -> str | None:
+    for key in keys:
+        value = data.get(key)
+        if value is not None:
+            return str(value)
+    return None
+
+
+def get_timestamp(data: dict[str, Any], keys: tuple[str, ...]) -> str:
+    if value := get_value(data, keys):
+        try:
+            timestamp = parse(value)
+            return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        except ParserError as e:
+            logger.debug("%s", e)
+            return value[0:19]
+    return ""
+
+
+def get_level(data: dict[str, Any], keys: tuple[str, ...]) -> str:
+    if value := get_value(data, keys):
+        return value.upper()
+    return ""
+
+
+@click.command()
+@click.argument("file", default="-", type=click.File("r"))
+@click.option(
+    "--timestamp",
+    "-t",
+    default=("asctime", "@timestamp", "timestamp"),
+    multiple=True,
+    help="Adds a key to detect the log time.",
+)
+@click.option(
+    "--level",
+    "-l",
+    default=("levelname", "level"),
+    multiple=True,
+    help="Adds a key to detect the log level.",
+)
+@click.option(
+    "--message",
+    "-m",
+    default=("message", "msg"),
+    multiple=True,
+    help="Adds a key to detect the log message.",
+)
+@click.option("--add", "-a", multiple=True, help="Additional value to display if present.")
+@click.option("--ignore", "-i", is_flag=True, help="Ignore non JSON logs")
+@click.option("--debug", is_flag=True, help="Turns on debug logs")
+def main(
+    file: IO[str],
+    timestamp: tuple[str, ...],
+    level: tuple[str, ...],
+    message: tuple[str, ...],
+    add: tuple[str, ...],
+    ignore: bool,
+    debug: bool,
+) -> None:
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+
+    # read in put line by line
+    for line in file:
+        # try to guess if the line might contain json
+        prefix, data = parse_json(line)
+
+        if data:
+            # extract log data
+            ts = get_timestamp(data, timestamp)
+            lvl = get_level(data, level)
+            msg = get_value(data, message) or ""
+
+            # format and output
+            prefix = click.style(prefix, fg="bright_black")
+            ts = click.style(ts.ljust(20), bold=True)
+            lvl_info = LOG_LEVELS.get(lvl, (lvl[0:5], "bright_magenta"))
+            lvl = click.style(lvl_info[0].rjust(5), fg=lvl_info[1], bold=True)
+            click.echo(f"{prefix}{ts}{lvl}: {msg}")
+
+            for key in add:
+                if value := data.get(key):
+                    name = click.style(key.rjust(25), fg="bright_black", bold=True)
+                    click.echo(f"{prefix}{name}: {value}")
+        elif not ignore:
+            click.echo(line, nl=False)
